4b89cd801ec8bab83a836c73cd42e821b08eaf01
suelmaparis <iscac11802@alumni.iscac.pt>
2025-05-16 07:00:28 -0400

    acrescentar botao legal services

--- teste.py
+++ teste.py
@@ -1,33 +1,5 @@
 import pandas as pd
-from country_continent_map import country_continent_map
 
-df = pd.read_csv('db/mom_world_dataset.csv', skiprows=4)
-# Lista de regiões que queremos excluir
-excluidos = [
-    'Africa Eastern and Southern',
-    'Africa Western and Central',
-    'Arab World',
-    'World',
-    'High income',
-    'Low income',
-    'Upper middle income',
-    'Lower middle income',
-    'Low & middle income',
-    'OECD members',
-    'Fragile and conflict affected situations',
-    'Latin America & Caribbean',
-    'Sub-Saharan Africa',
-    'Europe & Central Asia',
-    'Middle East & North Africa',
-    'South Asia',
-    'East Asia & Pacific'
-]
 
-df = df[~df['Country Name'].isin(excluidos)]
-
-df['Continent'] = df['Country Name'].map(country_continent_map)
-print(df['Continent'].head())
-print(df['Continent'].value_counts())
-
-\
-print(df.columns.tolist())
+df_unisef=pd.read_excel('db/UNICEF_Expanded_Global_Databases_ExclusiveBF_2024.xlsx', skiprows=4)
+print(df_unisef.info())
