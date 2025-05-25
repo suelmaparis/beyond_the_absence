5f8588477ef5b0e381fd67495b0a1e744535c8fd
suelmaparis <iscac11802@alumni.iscac.pt>
2025-05-25 18:00:01 -0400

    new blog

--- models.py
+++ models.py
@@ -3,13 +3,14 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    content = db.Column(db.Text)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
     likes = db.Column(db.Integer, default=0)
-    comments = db.relationship('Comment', backref='post', lazy=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
